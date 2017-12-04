677fef94a8ec230eca42b3143285588c5c4cff86
Adam Glaser <adm.glaser@gmail.com>
2017-12-04 20:31:12 +0100

    Renaming file.

--- RemoteServer/src/integration-test/java/selenium/GUITest.java
+++ RemoteServer/src/integration-test/java/selenium/GUITest.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class UITest {
+public class GUITest {
 
 	private static WebDriver driver;
 	
